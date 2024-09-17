43c808a6e6dd74af9d8ed697ebb6ab4d3eef5f25
ayleenrq <qisthyayleen@gmail.com>
2024-09-17 20:10:51 +0700

    P4_Praktikum 3: Penggunaan Operator PHP_Soal 3.1

---
+++ dasarWeb_Ayleen/P4/operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil dari perhitungan a = $a dan b = $b <br><br>";
+echo "Hasil penjumlahan (a dan b) = ". $hasilTambah. "<br>";
+echo "Hasil pengurangan (a dan b) = ". $hasilKurang. "<br>";
+echo "Hasil perkalian (a dan b) = ". $hasilKali. "<br>";
+echo "Hasil pembagian (a dan b) = ". $hasilBagi. "<br>";
+echo "Hasil sisa pembagian (a dan b) = ". $sisaBagi. "<br>";
+echo "Hasil pangkat (a dan b) = ". $pangkat. "<br>";
+?>
