c4fc308831fa0884b265a16d12b331745a696993
ayleenrq <qisthyayleen@gmail.com>
2024-09-17 20:27:34 +0700

    P4_Praktikum 3: Penggunaan Operator PHP_Soal 3.2

--- dasarWeb_Ayleen/P4/operator.php
+++ dasarWeb_Ayleen/P4/operator.php
@@ -16,4 +16,19 @@ echo "Hasil perkalian (a dan b) = ". $hasilKali. "<br>";
 echo "Hasil pembagian (a dan b) = ". $hasilBagi. "<br>";
 echo "Hasil sisa pembagian (a dan b) = ". $sisaBagi. "<br>";
 echo "Hasil pangkat (a dan b) = ". $pangkat. "<br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama= $a >= $b;
+
+echo "<br><br>Hasil dari perbandingan a = $a dan b = $b <br><br>";
+echo "Hasil (a) sama dengan (b) = $hasilSama <br>";
+echo "Hasil (a) tidak sama dengan (b) = $hasilTidakSama <br>";
+echo "Hasil (a) lebih kecil dari (b) = $hasilLebihKecil <br>";
+echo "Hasil (a) lebih besar dari (b) = $hasilLebihBesar <br>";
+echo "Hasil (a) lebih kecil sama dengan (b) = $hasilLebihKecilSama <br>";
+echo "Hasil (a) lebih besar sama dengan (b) = $hasilLebihBesarSama <br>";
 ?>
