43b904580c5b789109ce4ac1d44b5e529f9ceeae
ayleenrq <qisthyayleen@gmail.com>
2024-09-18 21:10:28 +0700

    P4_Praktikum 4: Penggunaan Struktur Kontrol Pada PHP_Cerita Game Skor_Soal 4.8

---
+++ dasarWeb_Ayleen/P4/cerita_gameScore.php
@@ -0,0 +1,7 @@
+<?php
+$point = 410;
+$hadiahTambahan = ($point > 500) ? "YA" : "TIDAK";
+
+echo "Total skor pemain adalah: $point <br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan";
+?>
