bf97f39bd464ae973f5bd05fac2a2df1242304cb
ayleenrq <qisthyayleen@gmail.com>
2024-09-24 09:29:14 +0700

    P5_PHP 2: Fungsi Rekursif_Perulangan For

--- dasarWeb_Ayleen/P5/PHP_2/rekursif.php
+++ dasarWeb_Ayleen/P5/PHP_2/rekursif.php
@@ -1,9 +1,13 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia! <br>";
+// function tampilkanHaloDunia() {
+//     echo "Halo dunia! <br>";
 
-    tampilkanHaloDunia();
+//     tampilkanHaloDunia();
+// }
+
+// tampilkanHaloDunia();
+
+for ($i = 1; $i <= 25; $i++) {
+    echo "Perulangan ke-{$i} <br>";
 }
-
-tampilkanHaloDunia();
 ?>
