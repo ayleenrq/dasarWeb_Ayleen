c26455205ebf08829c1e12b1e0088068112c2c8a
ayleenrq <qisthyayleen@gmail.com>
2024-10-01 15:11:00 +0700

    P6_Fungsi Parameter : Soal 5

--- dasarWeb_Ayleen/P6_PHP2/fungsi.php
+++ dasarWeb_Ayleen/P6_PHP2/fungsi.php
@@ -1,13 +1,19 @@
 <?php
-
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Ayleen<br/>";
+//membuat fungsi
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Hamdana", "Hallo");
 
+echo "<hr>";
+
+$saya = "Ayleen";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
