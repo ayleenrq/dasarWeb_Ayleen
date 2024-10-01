f24d16b7ae9fe3a7a888897560dc4fc8beb82d2c
ayleenrq <qisthyayleen@gmail.com>
2024-10-01 08:49:16 +0700

    P6_Praktikum 1: Indexed Array_Soal 1

---
+++ dasarWeb_Ayleen/P6_PHP2/array_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Array Terindeks</title>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+            $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+            echo $Listdosen[2] . "<br>";
+            echo $Listdosen[0] . "<br>";
+            echo $Listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
