cd5e5de7a2074183dab27c5533e77bb33f415a8f
ayleenrq <qisthyayleen@gmail.com>
2024-10-08 08:48:48 +0700

    P7_Praktikum 1: Function Isset_Soal 1.2

--- dasarWeb_Ayleen/P7/isset.php
+++ dasarWeb_Ayleen/P7/isset.php
@@ -7,4 +7,14 @@ if (isset($umur) && $umur >= 18) {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
 
+echo "<br><br>";
+
+$data = array("nama" => "Jane", "usia" => 25);
+
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+
 ?>
