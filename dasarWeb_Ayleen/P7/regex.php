60322c4b0b07145aef32f9fbdb67763e0e449210
ayleenrq <qisthyayleen@gmail.com>
2024-10-08 18:33:32 +0700

    P7_Praktikum 5: Penggunaan Regex Pada PHP_Soal 5.3

--- dasarWeb_Ayleen/P7/regex.php
+++ dasarWeb_Ayleen/P7/regex.php
@@ -18,7 +18,15 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Cocokkan: " . $matches[0];
 } else {
     echo "Tidak ada yang cocok!";
-
+    
 }
+
+echo"<br><br>";
+
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output: I like banana pie.
 
 ?>
