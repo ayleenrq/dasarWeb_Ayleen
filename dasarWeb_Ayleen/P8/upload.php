4a757e6fe97df9ec918cee448d0f5cfffda0e8bf
ayleenrq <qisthyayleen@gmail.com>
2024-10-15 10:21:17 +0700

    P8_Praktikum 1: Upload File_Soal 5

--- dasarWeb_Ayleen/P8/upload.php
+++ dasarWeb_Ayleen/P8/upload.php
@@ -9,7 +9,9 @@ if(isset($_POST["submit"])){
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br><br>";
+            // Menambahkan kode pada Soal 5
+            echo "<img src='" . $targetfile . "' width='200' style='height: auto;'>";
         } else {
             echo "Gagal mengunggah file.";
         }
